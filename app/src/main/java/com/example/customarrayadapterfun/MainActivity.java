1bffda81ba533d9760ca533503b0b8105b70204f
Brodhead <abrodhead@zagmail.gonzaga.edu>
2019-11-07 09:47:42 -0800

    finished

--- app/src/main/java/com/example/customarrayadapterfun/MainActivity.java
+++ app/src/main/java/com/example/customarrayadapterfun/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //create and populate contact list
-        List<Contact> contacts = new ArrayList<>();
+        final List<Contact> contacts = new ArrayList<>();
         contacts.add(new Contact("Bob", "(111)111-1111"));
         contacts.add(new Contact("Alice", "(222)222-2222"));
         contacts.add(new Contact());
@@ -34,12 +35,27 @@ public class MainActivity extends AppCompatActivity {
         //do it twice, once for s_l_i_1, once for s_l_i_2
 
         //create adapter, link it to listview
-        ArrayAdapter<Contact> contactArrayAdapter = new ArrayAdapter<Contact>(
-                this, android.R.layout.simple_list_item_1, contacts
+        ArrayAdapter<Contact> contactArrayAdapter = new ArrayAdapter<Contact>( //if there are multiple views, tell it
+                //which is the main view
+                this, android.R.layout.simple_list_item_2, android.R.id.text1, contacts
         ) {
+            //Inflate a layout for each row
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
-                return super.getView(position, convertView, parent);
+                View view = super.getView(position, convertView, parent);
+                Contact contact = contacts.get(position);
+                //we can set values for subviews of the view
+                //for s_l_i_1, no subview
+                /* for S_L_I_1
+                TextView tv1 = (TextView) view.findViewById(android.R.id.text1);
+                tv1.setText(contact.toString());
+                */
+                //2. simple list item 2
+                TextView tv1 = view.findViewById(android.R.id.text1);
+                TextView tv2 = view.findViewById(android.R.id.text2);
+                tv1.setText(contact.toString());
+                tv2.setText(contact.getPhoneNumber());
+                return view;
             }
         };
 
